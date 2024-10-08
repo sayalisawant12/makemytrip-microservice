623ffce5380777998c56941dbdca45d9904bb007
Your Name <sayalisawant.com>
2024-10-08 14:07:56 +0530

    codefactor

---
+++ src/main/java/com/example/makemytrip_microservice/flight.java
@@ -0,0 +1,10 @@
+package com.example.makemytrip_microservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class flight {
+    @GetMapping("/flight")
+    public String getData() {return  "Please book your flight ticket in Pune  10% discount" ; }
+}
